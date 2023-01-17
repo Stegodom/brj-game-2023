ab5f1c68e790c9ce6579b135f27a580aef0be3d6
MonkeMango <leepicrockyc@gmail.com>
2023-01-17 15:53:16 -0500

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float moveSpeed, jumpForce;
+
+    private Vector2 moveInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.Normalize();
+        
+        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
+    }
+}
